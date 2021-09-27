54ed2f3fbffe9af29c6de90ebd6fe5285576aed0
ammreem <ammar3dz@gmail.com>
2021-09-27 15:36:13 +0100

    update navbar : update links

--- src/components/layout@/navbar/navbar.jsx
+++ src/components/layout@/navbar/navbar.jsx
@@ -16,7 +16,20 @@ function header(props) {
     <header className="sticky z-50 shadow border-0  right-0 top-0 left-0 bg-white  unset-0  border">
       <nav>
         <div className="flex justify-between px-4 lg:px-8 items-center py-3">
-          <img src={logo} alt="logo" className="mr-2 " />
+          <div className="flex items-center">
+            <img src={logo} alt="logo" className="mr-2 " />
+            <div className="hidden md:block">
+              <span className="btn btn-primary ml-4 bg-gray-100 text-primary border-0 hover:bg-secondary hover:text-white">
+                home
+              </span>
+              <span className="btn btn-primary   bg-transparent text-primary border-0 hover:bg-secondary hover:text-white">
+                news
+              </span>
+              <span className="btn btn-primary  bg-transparent text-primary border-0 hover:bg-secondary hover:text-white">
+                about
+              </span>
+            </div>
+          </div>
           <div className="flex items-center">
             <div className="flex items-center border-2 rounded-md">
               <input
