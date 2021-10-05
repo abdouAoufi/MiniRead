5b5ddb287bffa20b3672e3d66cd9865e538ed595
AbdouAoufi <Abdouou7@gmail.com>
2021-10-05 14:14:43 +0100

    init :initialize the project

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function App() {
+  return <div>
+    <Title>Hello react</Title>
+  </div>;
+}
+
+const Title = styled.h1`
+  font-size: 1rem;
+  color : red;
+`;
+
+export default App;
