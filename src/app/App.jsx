7749f2820ac6b52e877db6350922ba3fb6893879
AbdouAoufi <Abdouou7@gmail.com>
2021-10-09 11:56:14 +0100

    markup: initialize home page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Article from "../pages/Article/Article";
+import Article from "../pages/Article";
+import Home from "../pages/Home";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Article} />
+        <Route path="/article" component={Article} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import { Box } from "@mui/system";
+
+function Home() {
+  return (
+    <Box component="div">
+      <Navbar />
+    </Box>
+  );
+}
+
+export default Home;
